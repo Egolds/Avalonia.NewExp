c5a4c0d4d242ae12258f30a616940d3a71607629
Egolds <egolds.dev@gmail.com>
2024-09-24 17:48:07 +0200

    +FocusBehavior

---
+++ Avalonia.NewExp/Core/Behaviors/FocusBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Avalonia.NewExp.Core.Behaviors
+{
+    public class FocusBehavior : AvaloniaObject
+    {
+        static FocusBehavior()
+        {
+            IsFocusedProperty.Changed.AddClassHandler<Interactive>(HandleIsFocusedChanged);
+        }
+
+        public static readonly AttachedProperty<bool> IsFocusedProperty =
+            AvaloniaProperty.RegisterAttached<FocusBehavior, StyledElement, bool>("IsFocused", false);
+
+        public static void SetIsFocused(StyledElement element, bool value) =>
+            element.SetValue(IsFocusedProperty, value);
+
+        public static bool GetIsFocused(StyledElement element) =>
+            element.GetValue(IsFocusedProperty);
+
+        private static void HandleIsFocusedChanged(Interactive interactive, AvaloniaPropertyChangedEventArgs args)
+        {
+            if (interactive is Control control && (bool)args.NewValue!)
+            {
+                if (control.Focusable && !control.IsFocused)
+                {
+                    control.Focus();
+                }
+            }
+        }
+    }
+}
--- Avalonia.NewExp/ViewModels/HomeViewModel.cs
+++ Avalonia.NewExp/ViewModels/HomeViewModel.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Avalonia.NewExp.ViewModels
 {
     public partial class HomeViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Home view!";
+
+        [ObservableProperty] private bool isTargetBtnFocused;
+
+        [RelayCommand]
+        private void LostFocus()
+        {
+            IsTargetBtnFocused = false;
+            IsTargetBtnFocused = true;
+        }
     }
 }
